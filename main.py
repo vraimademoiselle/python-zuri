4c5516d1ecb7726f7640485a2aceb962365e842f
Fatma Osman <99663006+vraimademoiselle@users.noreply.github.com>
2022-05-28 20:43:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-filename = "story.txt"
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
 def read_file_content(filename):
     # [assignment] Add your code here 
+
+
     # Open the file as f.
     # The function readlines() reads the file.
     with open(filename) as f:
@@ -15,26 +21,39 @@ def read_file_content(filename):
 
 # read_file_content(filename)
 
-def word_count():
+def count_words():
     text = read_file_content("./story.txt")
-    # text = read_file_content()
+    # [assignment] Add your code here
 
-    #cleaning
-    counts = dict()
-    words = text.split()
-    # words = [word.strip('.,!;()[]') for word in words]
-    # words = [word.replace("'s", '') for word in words]
+    return {"as": 10, "would": 20}
+    # Open the file in read mode
+text = open("story.txt", "r")
 
-    #finding unique
-    unique = []
-    for word in words:
-        if word not in unique:
-            unique.append(word)
+# Create an empty dictionary
+d = dict()
 
-    #sort
-    unique.sort()
+# Loop through each line of the file
+for line in text:
+	# Remove the leading spaces and newline character
+	line = line.strip()
 
-    #print
-    print(unique)
-    return counts
-word_count()
+	# Convert the characters in line to
+	# lowercase to avoid case mismatch
+	line = line.lower()
+
+	# Split the line into words
+	words = line.split(" ")
+
+	# Iterate over each word in line
+	for word in words:
+		# Check if the word is already in dictionary
+		if word in d:
+			# Increment count of word by 1
+			d[word] = d[word] + 1
+		else:
+			# Add the word to dictionary with count 1
+			d[word] = 1
+
+# Print the contents of dictionary
+for key in list(d.keys()):
+	print(key, ":", d[key])
